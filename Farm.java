3edd280b4857cf85da10a035c64415f608eabf08
dawong15 <dawong15@s.sfusd.edu>
2016-10-20 11:51:31 -0700

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -13,24 +13,3 @@
          System.out.println( "The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName() );    
     } 
 } 
-
-class Farm 
-{     
-   class Farm{
-   	private Animal[]aBunchOfAnimals=new Animal[3];
-   	public farm(){
-   		aBunchOfAnimals[0]=new NamedCow("cow","Bertha","moo");
-   		aBunchOfAnimals[1]=new Chick("chick","cheep","cluck");
-   		aBunchOfAnimals[2]=new Pig("pig","oink");
-   			
-   	}
-   	public void animalSounds(){
-   		for(int nI=0; nI<aBunchOfAnimals.length;nI++)
-   			System.out.println(aBunchOfAnimals[nI].getType+ "goes" +aBunchOfAnimals[nI].getSound());
-
-   	}
-   	System.out.println("the cow is known as"+((NamedCow)aBunchOfAnimals[0]).getName());
-   }
-}
-}
-
